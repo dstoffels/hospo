39f4131d5a5e58a9b39362aedc67ccdad1a2adcd
Danimal <dan.stoffels@gmail.com>
2024-10-11 17:48:19 -0400

    fix: update busstock page

--- app/busstock/page.tsx
+++ app/busstock/page.tsx
@@ -1,10 +1,15 @@
 import Page from '@/components/Page';
+import { Typography } from '@mui/material';
 import * as React from 'react';
 
 export type BusStockPageProps = object;
 
 const BusStockPage: React.FC<BusStockPageProps> = async ({}) => {
-	return <Page></Page>;
+	return (
+		<Page>
+			<Typography>Cuming Soon!</Typography>
+		</Page>
+	);
 };
 
 export default BusStockPage;
